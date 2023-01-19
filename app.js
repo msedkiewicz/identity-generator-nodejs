107d8a2e5b5742976063828749095cc54e2cd577
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-19 23:31:31 +0100

    Add people's random data

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const fs = require('fs');
+
+const genders = ['M', 'F'];
+const maleNames = ['John', 'Jack', 'Jose', 'Jesus', 'Tony', 'Tom', 'Timothy', 'Peter', 'Paul', 'Adam', 'Rob', 'Pep'];
+const femaleNames = ['Ann', 'Lena', 'Margaret', 'Jane', 'Magdalena', 'Justine', 'Katherine', 'Teresa', 'Elizabeth', 'Mary', 'Hedvig'];
+const lastNames = ['Doe', 'Smith', 'Richard', 'Downhill', 'Williamson', 'Hanks', 'Cruise', 'McTominay', 'Kane', 'Maguire', 'Guardiola'];
