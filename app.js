b97185809e916b48fd3e371543850c00b923b9a2
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-19 23:38:27 +0100

    Add code generating random identities

--- app.js
+++ app.js
@@ -1,6 +1,73 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const genders = ['M', 'F'];
-const maleNames = ['John', 'Jack', 'Jose', 'Jesus', 'Tony', 'Tom', 'Timothy', 'Peter', 'Paul', 'Adam', 'Rob', 'Pep'];
-const femaleNames = ['Ann', 'Lena', 'Margaret', 'Jane', 'Magdalena', 'Justine', 'Katherine', 'Teresa', 'Elizabeth', 'Mary', 'Hedvig'];
-const lastNames = ['Doe', 'Smith', 'Richard', 'Downhill', 'Williamson', 'Hanks', 'Cruise', 'McTominay', 'Kane', 'Maguire', 'Guardiola'];
+const genderChoices = ["M", "F"];
+const maleNames = [
+  "John",
+  "Jack",
+  "Jose",
+  "Jesus",
+  "Tony",
+  "Tom",
+  "Timothy",
+  "Peter",
+  "Paul",
+  "Adam",
+  "Rob",
+  "Pep",
+  "Harry",
+];
+const femaleNames = [
+  "Ann",
+  "Lena",
+  "Margaret",
+  "Jane",
+  "Magdalena",
+  "Justine",
+  "Katherine",
+  "Teresa",
+  "Elizabeth",
+  "Mary",
+  "Hedvig",
+];
+const lastNames = [
+  "Doe",
+  "Smith",
+  "Richard",
+  "Downhill",
+  "Williamson",
+  "Hanks",
+  "Cruise",
+  "McTominay",
+  "Kane",
+  "Maguire",
+  "Guardiola",
+];
+
+const randomChoice = (arr) => {
+  const randomNum = Math.floor(Math.random() * arr.length);
+  return randomNum;
+};
+
+const people = [];
+
+for (let i = 0; i < 20; i++) {
+  const person = {};
+  const gender = genderChoices[randomChoice(genderChoices)];
+  person.gender = gender;
+  if (gender === "M") person.name = maleNames[randomChoice(maleNames)];
+  if (gender === "F") person.name = femaleNames[randomChoice(femaleNames)];
+  person.lastName = lastNames[randomChoice(lastNames)];
+  person.age = Math.floor(Math.random() * 100 + 1);
+  people.push(person);
+}
+
+// console.log(people);
+
+const data = JSON.stringify(people);
+
+fs.writeFile("people.json", data, (err) => {
+  if (err) throw err;
+  console.log(people, "File has been successfully generated!");
+});
+1;
+people.json;
